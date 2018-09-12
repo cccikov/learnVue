705af8ba8e5b77f7a7a918fb4fa85b4a2ae94d29
ccc <522978139@qq.com>
2018-09-12 23:56:29 +0800

    组件间

--- vue-webpack/vue-cli-webpack/src/App.vue
+++ vue-webpack/vue-cli-webpack/src/App.vue
@@ -8,13 +8,19 @@
     </template>
     <img src="./assets/logo.png">
     <router-view/>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, distinctio eligendi fugiat possimus vero perferendis ut fuga pariatur, obcaecati nesciunt totam dolores adipisci atque sint dicta velit. Veniam, error omnis?</p>
+    <hi haha="这是哈哈属性"></hi>
+    <hello></hello>
   </div>
 </template>
 
 <script>
 // 以下就是除render和template的其他选项
+import hello from "./components/HelloWorld";
+import hi from "./components/hi";
 export default {
   name: "App",
+  components: { hello, hi },
   data: () => {
     return {
       name: "ccc"
