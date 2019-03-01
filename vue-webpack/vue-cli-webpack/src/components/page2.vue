0772606847562ea82927e33f035b14a1ed9a37d0
ccc <522978139@qq.com>
2019-03-01 18:32:43 +0800

    page

--- vue-webpack/vue-cli-webpack/src/components/page2.vue
+++ vue-webpack/vue-cli-webpack/src/components/page2.vue
@@ -1,10 +1,14 @@
 <template>
-    <div>
-       <p>page2</p>
+    <div class="wrap">
+        <div class="top">顶部</div>
+        <p>page2</p>
         <p v-for="val in ranInt(1,5)">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor repellat inventore error porro earum enim. Delectus, provident eum ab necessitatibus id mollitia eos? Ea dolorum repudiandae minus! Voluptatum, officia nemo!</p>
-         <p><a href="javascript:void(0)" @click="$router.push({'name':'page1'})">to page1</a></p>
-        <p><a href="javascript:void(0)" @click="$router.push({'name':'page3'})">to page3</a></p>
-
+        <p>
+            <a href="javascript:void(0)" @click="$router.push({'name':'page1'})">to page1</a>
+        </p>
+        <p>
+            <a href="javascript:void(0)" @click="$router.push({'name':'page3'})">to page3</a>
+        </p>
     </div>
 </template>
 <script>
@@ -21,3 +25,18 @@
         }
     };
 </script>
+<style lang="less" scoped>
+    .wrap {
+        padding: 40-24px 0 0;
+    }
+    .top {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        text-align: center;
+        background: #f00;
+        color: #fff;
+        line-height: 40px;
+    }
+</style>
--- vue-webpack/vue-cli-webpack/src/components/page3.vue
+++ vue-webpack/vue-cli-webpack/src/components/page3.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
+    <div class="wrap">
+        <div class="top">顶部</div>
         <p>page3</p>
         <p v-for="val in ranInt(10,20)">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor repellat inventore error porro earum enim. Delectus, provident eum ab necessitatibus id mollitia eos? Ea dolorum repudiandae minus! Voluptatum, officia nemo!</p>
         <p>
@@ -27,3 +28,18 @@
         }
     };
 </script>
+<style lang="less" scoped>
+    .wrap {
+        padding: 40-24px 0 0;
+    }
+    .top {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        text-align: center;
+        background: #f00;
+        color: #fff;
+        line-height: 40px;
+    }
+</style>
