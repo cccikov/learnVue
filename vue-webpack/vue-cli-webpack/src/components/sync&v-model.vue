16a60f8d0c1bc3fc2642bc6eb85a811cf569073d
ccc <522978139@qq.com>
2019-03-05 00:07:01 +0800

    sync 与 v-model

---
+++ vue-webpack/vue-cli-webpack/src/components/sync&v-model.vue
@@ -0,0 +1,61 @@
+<style lang="less" scoped>
+    div {
+        /deep/ p {
+            margin: 0 0 12px;
+        }
+    }
+</style>
+
+
+<template>
+    <div>
+        <div>
+            <p>{{json}}</p>
+            <p>{{arr}}</p>
+            <p>{{obj}}</p>
+        </div>
+        <div>
+            <!-- v-model 和 .sync 的区别就是 .sync 可以双向绑定多个值 -->
+            <sv v-model="json" :obj.sync="obj" :arr.sync="arr"></sv>
+        </div>
+    </div>
+</template>
+
+<script>
+    import sv from "./widget/sync&v-model-widget.vue";
+    export default {
+        components: {
+            sv
+        },
+        data() {
+            return {
+                str: "这是父组件的字符串",
+                num: 2934723894,
+                bool: true,
+                arr: [2, 3, 52, 34, 25, 234, 23, 5234, 156, 363],
+                obj: {
+                    name: "ccc",
+                    age: "18",
+                    id: "123123"
+                },
+                json: {
+                    list: [
+                        {
+                            name: "meme",
+                            value: 17
+                        },
+                        {
+                            name: "mumu",
+                            value: 123
+                        },
+                        {
+                            name: "ccc",
+                            value: 18
+                        }
+                    ],
+                    name:"json",
+                }
+            };
+        }
+    };
+</script>
---
+++ vue-webpack/vue-cli-webpack/src/components/widget/sync&v-model-widget.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <p @click="changeValue">{{value}}</p>
+        <p @click="changeArr">{{arr}}</p>
+        <p @click="changeObj">{{obj}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["value", "arr", "obj"],
+        methods: {
+            changeValue() {
+                // this.value.list.pop();
+                this.$emit("input", "v-model 同步");
+            },
+            changeArr() {
+                this.$emit("update:arr", "sync 同步");
+            },
+            changeObj() {
+                this.$emit("update:obj", "sync 同步");
+            }
+        }
+    };
+</script>
+
+<style lang="less" scoped>
+    div {
+        padding: 20px;
+        margin: 0 0 12px;
+        box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.3);
+    }
+</style>
