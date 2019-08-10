27cf9f4d85a85ac22bbf74da386e1fe42c3da9f6
ccc <522978139@qq.com>
2019-08-10 15:31:52 +0800

    readme

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 
 ### for learning vue
 
+vue-webpack 里面项目是通过 vue-cli2 脚手架 ，参考 https://github.com/vuejs/vue-cli/tree/v2
+
+``` bash
+$ vue init <template-name> <project-name>
+```
+
+``` bash
+$ vue init webpack my-project
+```
+
+`webpack` - A full-featured Webpack + vue-loader setup with hot reload, linting, testing & css extraction.
+
+`webpack-simple` - A simple Webpack + vue-loader setup for quick prototyping.
+
 #### 附带
 
 * vuex
--- vue-webpack/vue-cli-webpack/src/components/standard.vue
+++ vue-webpack/vue-cli-webpack/src/components/standard.vue
@@ -1,9 +1,29 @@
+<!-- vue规范模板，第一行要注释表明当前组件的的作用 -->
 <template>
-    <div>Lorem, ipsum dolor.</div>
+    <div>
+        <Tabs>
+            <TabPane label="window">
+                <window>window组件</window>
+            </TabPane>
+            <TabPane label="mac">
+                <mac>mac组件</mac>
+            </TabPane>
+            <TabPane label="Ubuntu">
+                <Ubuntu>Ubuntu组件</Ubuntu>
+            </TabPane>
+            <TabPane label="android">
+                <android>android组件</android>
+            </TabPane>
+            <TabPane label="ios">
+                <ios>ios组件</ios>
+            </TabPane>
+        </Tabs>
+    </div>
 </template>
 <script>
     export default {
         /**
+         * vue 选项按照以下顺序
          * props
          * data
          * methods
@@ -19,9 +39,11 @@
          * mounted
          * destroyed
          */
-        props:[],
+        props: [],
         data() {
             return {
+                /* 每个变量后面都要有注释表明这个变量的作用 */
+
                 select: [], // 选中的块，原始数据，绑定数据
                 sortData: {}, // 整理好的数据
                 timeData: {}, // 转化为时间后的数据
@@ -29,13 +51,15 @@
                 twoPoint: [], // 记录选择的两个点（或者点击的一个点）
 
                 /* 禁止在变量后面加上数字来区分变量 */
-                data1:{}, // xxx数据
-                data2:{} // xxx数据
+                data1: {}, // xxx数据
+                data2: {} // xxx数据
             };
         },
         methods: {
             /**
-             * init 初始化
+             * init 初始化；
+             * 不要有复杂的逻辑，基本就是方法method调用，或者变量赋值；
+             * 一旦有复杂的逻辑，可封装成一个新的方法；
              */
             init() {
                 this.getData();
@@ -60,13 +84,15 @@
              * @param {String} id id
              * @return {Array<String>} 结果
              */
-            getData(id) {},
+            getData(id) {}
         },
         computed: {},
         watch: {},
-        components:{},
+        components: {},
+
         beforeCreate() {},
         created() {
+            // 也不要有复杂逻辑，基本就是调用初始化方法 init
             this.init();
         },
         mounted() {},
